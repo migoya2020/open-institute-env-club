5d6bb78aaca0706fa685dc59410de433d801244b
David Migoya <dmigoya@gmail.com>
2021-04-29 16:00:39 +0300

    added login and  logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 "
+    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3"
   >
     <div
       class="container px-4 mx-auto flex flex-wrap items-center justify-between"
@@ -22,42 +22,60 @@
       </div>
       <div
         class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none"
-        v-bind:class="{'hidden': !showMenu, 'block': showMenu}"
+        v-bind:class="{ hidden: !showMenu, block: showMenu }"
       >
-
-<!-- Nav Items begin -->
-      <div class="flex flex-col lg:flex-row  ml-1 items-center" id="navbar-info">
+        <!-- Nav Items begin -->
+        <div
+          class="flex flex-col lg:flex-row ml-1 items-center"
+          id="navbar-info"
+        >
           <ul class="flex flex-col lg:flex-row list-none ml-auto items-start">
             <li class="nav-item">
-              <a class="px-3 py-2 flex items-center text-lg   font-bold leading-snug lg:text-white lg:hover:text-indigo-600 text-gray-800 hover:opacity-75" href="#" @click="$router.push('/about')">
+              <a
+                class="px-3 py-2 flex items-center text-lg font-bold leading-snug lg:text-white lg:hover:text-indigo-600 text-gray-800 hover:opacity-75"
+                href="#"
+                @click="$router.push('/about')"
+              >
                 About
               </a>
             </li>
             <li class="nav-item">
-              <a class="px-3 py-2 flex items-center text-lg   font-bold leading-snug lg:text-white md:hover:text-indigo-500 text-gray-800 hover:opacity-75" href="#"  @click="$router.push('/programmes')">
+              <a
+                class="px-3 py-2 flex items-center text-lg font-bold leading-snug lg:text-white md:hover:text-indigo-500 text-gray-800 hover:opacity-75"
+                href="#"
+                @click="$router.push('/programmes')"
+              >
                 Programs
               </a>
             </li>
             <li class="nav-item">
-              <a class="px-3 py-2 flex items-center text-lg  font-bold leading-snug lg:text-white lg:hover:text-indigo-500 text-gray-800 hover:opacity-75" href="#" @click="$router.push('/cohorts')">
-                Cohorts 
+              <a
+                class="px-3 py-2 flex items-center text-lg font-bold leading-snug lg:text-white lg:hover:text-indigo-500 text-gray-800 hover:opacity-75"
+                href="#"
+                @click="$router.push('/cohorts')"
+              >
+                Cohorts
               </a>
             </li>
             <li class="nav-item">
-              <a class="px-3 py-2 flex items-center text-lg  font-bold leading-snug lg:text-white lg:hover:text-indigo-600 text-gray-800 hover:opacity-75" href="#" @click="$router.push('/gallary')">
+              <a
+                class="px-3 py-2 flex items-center text-lg font-bold leading-snug lg:text-white lg:hover:text-indigo-600 text-gray-800 hover:opacity-75"
+                href="#"
+                @click="$router.push('/gallary')"
+              >
                 Gallary
               </a>
             </li>
           </ul>
         </div>
-        
+
         <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
           <li class="flex items-center">
             <a
               class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="#Open-Institute-club"
               ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-facebook text-lg leading-lg "
+                class="lg:text-gray-300 text-gray-500 fab fa-facebook text-lg leading-lg"
               ></i
               ><span class="lg:hidden inline-block ml-2">Share</span></a
             >
@@ -67,7 +85,7 @@
               class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="#Open-Institute-club"
               ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-twitter text-lg leading-lg "
+                class="lg:text-gray-300 text-gray-500 fab fa-twitter text-lg leading-lg"
               ></i
               ><span class="lg:hidden inline-block ml-2">Tweet</span></a
             >
@@ -77,18 +95,32 @@
               class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
               href="#Open-Institute-club"
               ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg "
+                class="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg"
               ></i
               ><span class="lg:hidden inline-block ml-2">Star</span></a
             >
           </li>
           <li class="flex items-center px-2">
- 
-          <button class="inline-flex text-white bg-indigo-500 font-semibold border border-white py-1 px-4 focus:outline-none hover:text-indigo hover:bg-white rounded" @click="$router.push('/login')">Sign in</button>
-          </li >
+            <button v-if="!user"
+              class="inline-flex text-white bg-indigo-500 font-semibold border border-white py-1 px-4 focus:outline-none hover:text-indigo hover:bg-white rounded"
+              @click="$router.push('/login')"
+            >
+              Sign in
+            </button>
+          </li>
           <li class="flex items-center px-2">
- 
-          <button class="inline-flex bg-transparent hover:bg-indigo-500 text-white font-semibold hover:text-white py-1 px-4 border border-indigo-500 hover:border-transparent rounded" @click="$router.push('/register')">Register</button>
+            <button v-if="!user"
+              class="inline-flex bg-transparent hover:bg-indigo-500 text-white font-semibold hover:text-white py-1 px-4 border border-indigo-500 hover:border-transparent rounded"
+              @click="$router.push('/register')"
+            >
+              Register
+            </button>
+<button v-if="user" type="submit" @click="logOut()"
+              class="inline-flex bg-transparent hover:bg-indigo-500 text-white font-semibold hover:text-white py-1 px-4 border border-indigo-500 hover:border-transparent rounded"
+            >
+              Logout
+            </button>
+ <h2 class="px-4 text-white font-semibold" v-if="user" >{{user}}</h2>
           </li>
         </ul>
       </div>
@@ -96,17 +128,40 @@
   </nav>
 </template>
 <script>
+import firebase from 'firebase'
 export default {
- name: "Navbar",
+  name: "Navbar",
   data() {
     return {
-      showMenu: false
-    }
+      user: null,
+      showMenu: false,
+    };
+  },
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+ 
+        this.user = user.email;
+      } else {
+        this.user = null;
+      }
+    });
   },
   methods: {
-    toggleNavbar: function(){
+    toggleNavbar: function () {
       this.showMenu = !this.showMenu;
-    }
-  }
-}
+    },
+    logOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          firebase.auth().onAuthStateChanged(() => {
+// Redirect after  logout
+            this.$router.push("/landing");
+          });
+        });
+    },
+  },
+};
 </script>
