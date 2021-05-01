688d9075a14a856dc545474b5d967a12451fa931
David Migoya <dmigoya@gmail.com>
2021-05-01 09:17:01 +0300

    .

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -42,7 +42,7 @@
 			<div class="flex-1 h-px sm:w-16 bg-coolGray-300"></div>
 		</div>
 		<div class="flex justify-center space-x-4">
-			<button aria-label="Log in with Google" class="p-3 rounded-sm">
+			<button  @click="socialregister()" aria-label="register with Google" class="p-3 rounded-sm">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="w-5 h-5 fill-current">
 					<path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
 				</svg>
@@ -96,6 +96,14 @@ methods: {
         alert(error.message);
       });
   },
+socialregister(){
+const provider  = new  firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(() => {
+ alert('Successfully logged in. we are redirecting you');
+this.$router.replace("/landing");
+}).catch((error)  => { alert(error.message)});
+  
+}
 },
 };
 </script>
